82d8f98b83b8b9322c76717a2deaec602b82aff0
Apple <apple@apples-imac.local>
2016-04-12 11:13:17 +0500

    Initial Commit

---
+++ Ghana Live TV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ghana Live TV
+//
+//  Created by Abid Ali on 4/12/16.
+//  Copyright © 2016 Abid Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ghana Live TVTests/Ghana_Live_TVTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ghana_Live_TVTests.swift
+//  Ghana Live TVTests
+//
+//  Created by Abid Ali on 4/12/16.
+//  Copyright © 2016 Abid Ali. All rights reserved.
+//
+
+import XCTest
+@testable import Ghana_Live_TV
+
+class Ghana_Live_TVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
